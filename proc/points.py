896c31d1c63a016850512bba04dcf6ec2a3ee8cb
Vitalii Abetkin <abvit89@gmail.com>
2017-12-15 19:14:53 +0300

    .

--- proc/points.py
+++ proc/points.py
@@ -30,10 +30,11 @@ class Parser:
         for m in re.finditer(self.SIMPLE_REG, text):
             li.append(self.to_simple_point(m))
 
-        return sorted(
+        self.points = sorted(
             self.dia_points + self.regular_points,
             key=lambda p: p['start']
         )
+        return self.points
 
     def to_simple_point(self, match):
         return {
@@ -53,26 +54,88 @@ class Parser:
         # getting full info:
         # split by spaces
         after_text = self.text[dic['end']:dic['end']+20]
-        chunks = after_text.split()
+        # chunks = after_text.split()
         # find 1st non-match
-        last_match = None
+        # last_match = None
         reg = r'([RF])(-?[\d.]+)'
-        for chunk in chunks:
-            m = re.match(reg, chunk)
-            if m:
-                last_match = m
-                dic[m.group(1)] = m.group(2)
-        if last_match:
-            dic['end'] += last_match.end()
+        # for i, chunk in enumerate(chunks):
+        #     m = re.match(reg, chunk)
+        #     if not m:
+        #         break
+        #     prev = chunks[i-1]
+        #     ''.join(chunks[:i]
+        #     dic[m.group(1)] = m.group(2)
+        # if last_match:
+        #     dic['end'] += last_match.end()
+        # return dic
+
+        prev_m = None
+        for m in re.finditer(reg, after_text):
+            if prev_m and after_text[prev_m.end():m.start()].strip():
+                break
+            prev_m = m
+        if prev_m is not None:
+            dic['end'] += prev_m.end()
         return dic
+
+    # def _():
+    #     reg = r""
+
+    def render(self):
+        points = [p for p in self.points if p.get('is_dia')]
+        upper = len(self.text)
+        intervals = inverse_intervals(points, upper)
+        def _render(p):
+            return f"X{p['X']} Z{p['Z']} R{p['R']} F{p['F']}"
+        li = []
+        for (start, end), p in zip(
+            intervals,
+            points + [None]
+        ):
+            li.append(self.text[start:end])
+            if p is None:
+                continue
+            li.append(_render(p))
+        return ''.join(li)
 
 
 class DiaCalculator:
 
     def __init__(self, points):
         self.points = points
+        self.state = {}
 
     def run(self):
-        for p in self.points:
-            if p.get('is_dia'):
-                self.handle_dia(p)
+        for i, p in enumerate(self.points):
+
+            if not p.get('is_dia'):
+                self.state.update(p)
+            else:
+                self.handle_dia(p, dict(self.state))
+
+    
+    def handle_dia(self, p, prev):
+        import math
+        d = math.sqrt(
+            (p['X'] - prev['X']) ** 2 + (p['Z'] - prev['Z']) ** 2
+        )
+        if d > 1:
+            p['X'] //= 2
+
+
+def inverse_intervals(points, upper_bound):
+    li = [0]
+    for p in points:
+        li.append(p['start'])
+        li.append(p['end'])
+    li.append(upper_bound - 1)
+    def pairwise(li):
+        from itertools import tee
+        a, b = tee(li)
+        next(b, None)
+        return zip(a, b)
+    pairs = []
+    for i, p in enumerate(pairwise(li)):
+        if i % 2 == 0:
+            pairs.append(p)
+    return pairs
--- test_points.py
+++ test_points.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from proc.points import Parser
+from proc.points import Parser, inverse_intervals
 
 @pytest.fixture()
 def parser():
@@ -15,6 +15,38 @@ X1 Z-2 R1 F100
 
 def test_parser(parser):
     points = parser.parse()
+    
     assert len(parser.dia_points) == 2
     assert len(parser.regular_points) == 2
     assert {p['X'] for p in points} == {'23', '.3', '.4', '1'}
+
+def test_inverse():
+    points = [
+        {'start': 1, 'end': 2},
+        {'start': 3, 'end': 4},
+    ]
+    upper = 6
+    r = inverse_intervals(points, upper)
+    assert r == [(0, 1), (2, 3), (4, 5)]
+
+def test_no_change(parser):
+    points = parser.parse()
+    text = parser.render()
+    print(text)
+    print()
+    print()
+    assert parser.text == text
+
+def test_cut(parser):
+    text = parser.text
+    li = []
+    points = [
+        p for p in parser.parse()
+        if p.get('is_dia')
+    ]
+    for start, end in inverse_intervals(points, len(text)):
+        li.append(
+            text[start:end]
+        )
+    new_text = ''.join(li)
+    assert new_text.strip() == "X.3 X.4 some text"
